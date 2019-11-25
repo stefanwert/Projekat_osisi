9dde8d2ca2efb7b2ce71944278e1a8ed8a731f63
stefanwert <58173887+stefanwert@users.noreply.github.com>
2019-11-25 15:10:48 +0100

    Predmet i profesor

---
+++ src/java_oisisi/Predmet.java
@@ -0,0 +1,10 @@
+package java_oisisi;
+
+public class Predmet {
+	String sifraPredmeta;
+	String nazivPredmeta;
+	int semestar;
+	int godinaStudija;
+	Profesor profesor;
+	//fali jos spisak sutdenata 
+}
--- src/java_oisisi/Profesor.java
+++ src/java_oisisi/Profesor.java
@@ -1,5 +1,20 @@
 package java_oisisi;
 
-public class Profesor {
+import java.util.*;
+import java.sql.Date;
 
+
+public class Profesor {
+	String ime;
+	String prezime;
+	Date datum;
+	String adresaStanovanja;
+	String kontaktTel;
+	String email;
+	String adresaKancelarije;
+	String brLicneKarte;
+	String titula;
+	String zvanje;
+	List<String> ListaPredmeta;
+	
 }
