378dee63681de1a5f815746afddc6eec19e051c9
stefanwert <58173887+stefanwert@users.noreply.github.com>
2019-11-25 16:35:06 +0100

    dodaj predmet,obrisi predmet , izmeni predmet u klasi studenska sluzba

--- src/model/Predmet.java
+++ src/model/Predmet.java
@@ -8,13 +8,68 @@ public class Predmet {
 	int godinaStudija;
 	Profesor profesor;
 	List<Student> listaStudenata;
-	public Predmet(String sifraPredmeta, String nazivPredmeta, int semestar, int godinaStudija, Profesor profesor,
-			List<Student> listaStudenata) {
+	public Predmet() {
+		sifraPredmeta="";
+		nazivPredmeta="";
+		semestar=0;
+		godinaStudija=0;
+		profesor=null;
+		listaStudenata=new ArrayList<Student>();
+	}
+	public Predmet(String sifraPredmeta, String nazivPredmeta, int semestar, int godinaStudija, Profesor profesor,List<Student> listaStudenata) {
 		this.sifraPredmeta = sifraPredmeta;
 		this.nazivPredmeta = nazivPredmeta;
 		this.semestar = semestar;
 		this.godinaStudija = godinaStudija;
 		this.profesor = profesor;
+		this.listaStudenata = listaStudenata;
+	}
+
+	public String getSifraPredmeta() {
+		return sifraPredmeta;
+	}
+
+	public void setSifraPredmeta(String sifraPredmeta) {
+		this.sifraPredmeta = sifraPredmeta;
+	}
+
+	public String getNazivPredmeta() {
+		return nazivPredmeta;
+	}
+
+	public void setNazivPredmeta(String nazivPredmeta) {
+		this.nazivPredmeta = nazivPredmeta;
+	}
+
+	public int getSemestar() {
+		return semestar;
+	}
+
+	public void setSemestar(int semestar) {
+		this.semestar = semestar;
+	}
+
+	public int getGodinaStudija() {
+		return godinaStudija;
+	}
+
+	public void setGodinaStudija(int godinaStudija) {
+		this.godinaStudija = godinaStudija;
+	}
+
+	public Profesor getProfesor() {
+		return profesor;
+	}
+
+	public void setProfesor(Profesor profesor) {
+		this.profesor = profesor;
+	}
+
+	public List<Student> getListaStudenata() {
+		return listaStudenata;
+	}
+
+	public void setListaStudenata(List<Student> listaStudenata) {
 		this.listaStudenata = listaStudenata;
 	}
 	
--- src/model/Studenska_sluzba.java
+++ src/model/Studenska_sluzba.java
@@ -2,7 +2,34 @@ package model;
 import java.util.*;
 
 public class Studenska_sluzba {
-	List<Student> listaStudenata;
-	List<Predmet> listaPredmeta;
-	List<Profesor> listaProfesora;
+	List<Student> listaStudenata=new ArrayList<Student>();
+	List<Predmet> listaPredmeta=new ArrayList<Predmet>();
+	List<Profesor> listaProfesora=new ArrayList<Profesor>();
+	
+	public Studenska_sluzba(List<Student> listaStudenata, List<Predmet> listaPredmeta, List<Profesor> listaProfesora) {
+		this.listaStudenata = listaStudenata;
+		this.listaPredmeta = listaPredmeta;
+		this.listaProfesora = listaProfesora;
+	}
+	void dodaj_predmet(String sifraPredmeta, String nazivPredmeta, int semestar, int godinaStudija, Profesor profesor,List<Student> listaStudenata) {
+		Predmet p=new Predmet( sifraPredmeta,  nazivPredmeta,  semestar,  godinaStudija,  profesor, listaStudenata);
+		listaPredmeta.add(p);
+	}
+	boolean obrisi_predmet(String sifraPredmeta) {
+		for (Predmet predmet : listaPredmeta) {
+			if(sifraPredmeta.equals(predmet.getSifraPredmeta())) {
+				listaPredmeta.remove(predmet);
+				return true;
+			}
+		}
+		return false;
+	}
+	void izmeni_predmet(String sifraPredmeta, String nazivPredmeta, int semestar, int godinaStudija, Profesor profesor,List<Student> listaStudenata,Predmet stari) {
+		stari.setSifraPredmeta(sifraPredmeta);
+		stari.setNazivPredmeta(nazivPredmeta);
+		stari.setSemestar(semestar);
+		stari.setGodinaStudija(godinaStudija);
+		stari.setProfesor(profesor);
+		stari.setListaStudenata(listaStudenata);
+	}
 }
