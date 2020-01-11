1e25cf363c84a946e4ae8a4913e3b8f43a19d4df
NemanjaTam <57495945+NemanjaTam@users.noreply.github.com>
2020-01-11 20:01:50 +0100

    Proba

--- src/pogled/Help.java
+++ src/pogled/Help.java
@@ -28,34 +28,32 @@ public class Help extends JFrame {
 			instance=null;
 		}
 		
-		private Help(){}
-		
-		public void call(){
-			Toolkit kit = Toolkit.getDefaultToolkit();
-	        Dimension screenSize = kit.getScreenSize();
-	        int screenHeight = screenSize.height;
-	        int screenWidth = screenSize.width;
-	        setSize(screenWidth / 2, screenHeight / 2);
-	        setTitle("Help");
-	        setLocationRelativeTo(null);
-	        setVisible(true);
-	        
-	        JPanel txtPanel = new JPanel(new BorderLayout(10,5));
-	        txtPanel.setBackground(Color.LIGHT_GRAY);
-	        
-	        JLabel label = new JLabel("Detaljan opis o naèinu korišæenja aplikacije");
-	        label.setPreferredSize(new Dimension(20, 20));
-	        txtPanel.add(label, BorderLayout.NORTH);
-	        
-	        // viselinijsko tekstualno polje
-	        JTextArea textArea = new JTextArea("Ovo je viselinijsko tekstualno polje");
-	        // dodajemo scroller-e po potrebi
-	        JScrollPane scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-	        
-	        txtPanel.add(scrollPane, BorderLayout.CENTER);
-			
-	        this.add(txtPanel);
-	       
+		private Help(){
+				Toolkit kit = Toolkit.getDefaultToolkit();
+		        Dimension screenSize = kit.getScreenSize();
+		        int screenHeight = screenSize.height;
+		        int screenWidth = screenSize.width;
+		        setSize(screenWidth / 2, screenHeight / 2);
+		        setTitle("Help");
+		        setLocationRelativeTo(null);
+		        setVisible(true);
+		        
+		        JPanel txtPanel = new JPanel(new BorderLayout(10,5));
+		        txtPanel.setBackground(Color.LIGHT_GRAY);
+		        
+		        JLabel label = new JLabel("Detaljan opis o naèinu korišæenja aplikacije");
+		        label.setPreferredSize(new Dimension(20, 20));
+		        txtPanel.add(label, BorderLayout.NORTH);
+		        
+		        // viselinijsko tekstualno polje
+		        JTextArea textArea = new JTextArea("Ovo je viselinijsko tekstualno polje");
+		        // dodajemo scroller-e po potrebi
+		        JScrollPane scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		        
+		        txtPanel.add(scrollPane, BorderLayout.CENTER);
+				
+		        this.add(txtPanel);
+		       
 		}
 		
 }
